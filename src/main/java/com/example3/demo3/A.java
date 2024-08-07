60b2161a8b9febffa748c888832319a65cad793b
akash <mraka@Venom>
2024-08-07 11:43:36 +0530

    added new A class

---
+++ src/main/java/com/example3/demo3/A.java
@@ -0,0 +1,11 @@
+package com.example3.demo3;
+
+public class A {
+    public void  add(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+       A a = new A();
+       a.add(1,5);
+    }
+}
